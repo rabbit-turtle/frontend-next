7cb81d070c6b9e67cd6c450e54ddf2d1c7bec59f
mengkki <guswnl0610@naver.com>
2021-03-28 20:46:40 +0900

    Add : 어긋난 ui 수정

--- components/MapNavigationBar/index.tsx
+++ components/MapNavigationBar/index.tsx
@@ -1,18 +1,23 @@
+import { memo } from 'react';
 import { useRouter } from 'next/router';
 import { Location } from 'react-ionicons';
 import Link from 'next/link';
 import { ROUTES } from 'constants/index';
 import ProgressBar from 'components/ProgressBar';
+import dayjs from 'dayjs';
 
-function MapNavigationBar({ title }: { title: string }) {
+function MapNavigationBar({ title, reserved_time }: { title: string; reserved_time: string }) {
   const router = useRouter();
 
   return (
     <Link href={`${ROUTES.map}/[ROOM_ID]`} as={`${ROUTES.map}/${router.query.ROOM_ID}`}>
-      <div className="2xl:w-512 top-13 w-full flex items-center justify-between py-3 px-7 bg-white border-t border-gray-200 cursor-pointer">
+      <div className="w-full flex items-center justify-between py-1 px-7 bg-white border-t border-gray-200 cursor-pointer">
         <Location color="#b2dfdb" height="37px" width="37px" />
         <div className="w-4/5">
-          <p className="mt-4 text-lg">{title}</p>
+          <p className="text-md truncate">{title}</p>
+          <p className="text-primary-dark">
+            {dayjs(reserved_time).format('YYYY년 M월 D일 h:mm a')}
+          </p>
           <ProgressBar type="distance" value={40} />
         </div>
       </div>
@@ -20,4 +25,4 @@ function MapNavigationBar({ title }: { title: string }) {
   );
 }
 
-export default MapNavigationBar;
+export default memo(MapNavigationBar);
